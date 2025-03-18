b84501cc0ca960d3f009b4d3b999a7670b79be0d
rbtechme <rbtechme@gmail.com>
2025-03-18 16:43:05 +0530

    add useeffect to call premiumverify method

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React from "react";
+import React, { use } from "react";
 import BASE_URL from "../constants/baseUrl";
 
 const Premium = () => {
@@ -49,6 +49,10 @@ const Premium = () => {
       setIsPremium(false);
     }
   };
+
+  useEffect(() => {
+    premiumVerify();
+  }, []);
 
   return isPremium ? (
     <>
